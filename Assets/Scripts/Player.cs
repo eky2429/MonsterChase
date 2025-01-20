43c6e1112ac9245f6f36d2c017ef5f524dde2543
eky2429 <eky2429@gmail.com>
2025-01-20 11:43:42 -0600

    Added 2 players (prefabs, animations, and code)

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float moveForce = 10f;
+    
+    [SerializeField]
+    private float jumpForce = 11f;
+
+    private float movementX;
+
+    private Rigidbody2D myBody;
+    private SpriteRenderer sr;
+    private Animator anim;
+    private string WALK_ANIMATION = "isMoving";
+
+
+    //Called as soon as object is created
+    private void Awake() {
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMoveKeyboard();
+        AnimatePlayer();
+    }
+
+    void PlayerMoveKeyboard() {
+        movementX = Input.GetAxisRaw("Horizontal");
+
+        transform.position += new Vector3(movementX, 0f, 0f) * Time.deltaTime * moveForce;
+    }
+
+    void AnimatePlayer() {
+        if (movementX > 0) {
+            anim.SetBool(WALK_ANIMATION, true);
+            sr.flipX = false;
+        } else if (movementX < 0) {
+            anim.SetBool(WALK_ANIMATION, true);
+            sr.flipX = true;
+        } else {
+            anim.SetBool(WALK_ANIMATION, false);
+        }
+    }
+}
